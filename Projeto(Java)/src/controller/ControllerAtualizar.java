b3494646ec6f9848b2ea7f8d0cb3abb2fa014694
Nuno Martins <145809068+nunomgs136@users.noreply.github.com>
2024-05-22 19:48:08 -0300

    Controller de atualizar(antes de testar)

---
+++ Projeto(Java)/src/controller/ControllerAtualizar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+import DAO.UsuarioDAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.*;
+/**
+ *
+ * @author unifnsilva
+ */
+public class ControllerAtualizar {
+    private Connection conn;
+    private UsuarioDAO dao = new UsuarioDAO(conn);
+    
+}
